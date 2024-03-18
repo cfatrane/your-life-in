9e95986db415106a0d31b8c1a649d26292ba550a
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-18 20:48:25 +0100

    start the app dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,113 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-import Button from '@/components/Button';
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs, { Dayjs } from "dayjs";
 
-import reactLogo from './assets/react.svg';
+import Day from "@/components/Day";
+import "./App.css";
 
-import './App.css';
+const NUMBER_CASE_FOR_WEEKS = 52 * 90;
+const NUMBER_CASE_FOR_YEARS = 9 * 10;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [period, setPeriod] = useState<string>("weeks");
+  const [abscissa, setAbscissa] = useState<number>(52);
+  // const [ordinate, setOrdinate] = useState<number>(90);
+  const [date, setDate] = useState<Dayjs | null>(null);
+  const [daysAlive, setDaysAlive] = useState<number>(0);
+  const [weeksAlive, setWeeksAlive] = useState<number>(0);
+  const [caseNumber, setCaseNumber] = useState<number>(NUMBER_CASE_FOR_WEEKS);
+
+  useEffect(() => {
+    if (period === "years") {
+      setCaseNumber(NUMBER_CASE_FOR_YEARS);
+      setAbscissa(9);
+    }
+    // else if (period === 'months') {
+    //   setAbscissa(90);
+    //   setOrdinate(52);
+    // }
+    else if (period === "weeks") {
+      setCaseNumber(NUMBER_CASE_FOR_WEEKS);
+      setAbscissa(52);
+    }
+    // return () => {};
+  }, [period]);
+
+  useEffect(() => {
+    if (date) {
+      const result = dayjs().diff(dayjs(date), "day");
+      const weeksDiff = dayjs().diff(dayjs(date), "week");
+      setDaysAlive(result);
+      setWeeksAlive(weeksDiff);
+    }
+
+    // return () => {};
+  }, [date]);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setPeriod(event.target.value as string);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img alt="React logo" className="logo react" src={reactLogo} />
-        </a>
+    <div>
+      <div className="flex flex-col items-center">
+        <div className="flex">
+          <h1>Your Life In</h1>
+
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <Select
+                id="demo-simple-select"
+                label="Period"
+                labelId="demo-simple-select-label"
+                onChange={handleChange}
+                value={period}>
+                <MenuItem value={"years"}>Years</MenuItem>
+
+                <MenuItem value={"months"}>Months</MenuItem>
+
+                <MenuItem value={"weeks"}>Weeks</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DatePicker"]}>
+            <DatePicker
+              format="DD/MM/YYYY"
+              onAccept={(newValue) => setDate(newValue)}
+              // onChange={(newValue) => setDate(newValue)}
+              value={date}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
       </div>
 
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div
+        className={`grid gap-1`}
+        style={{ gridTemplateColumns: `repeat(${abscissa}, minmax(0, 1fr))` }}>
+        {Array.from({ length: caseNumber }).map((_, index) => (
+          <>
+            <Day isActive={index < weeksAlive} key={index} weekNumber={index} />
+          </>
+        ))}
       </div>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Inspired by{" "}
+        <a href="https://waitbutwhy.com/2014/05/life-weeks.html">WaitButWhy</a>
       </p>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Day/index.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+import Tooltip from "@mui/material/Tooltip";
+
+type Props = { isActive: boolean; weekNumber: any };
+
+function Day({ isActive, weekNumber }: Props) {
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    if (weekNumber <= 52) {
+      setTitle(`${weekNumber} weeks`);
+    } else if (weekNumber > 52) {
+      setTitle(
+        `${Math.trunc(weekNumber / 52)} years and ${weekNumber % 52} weeks`
+      );
+    }
+  }, []);
+
+  return (
+    <Tooltip title={title}>
+      <div
+        onMouseEnter={() => {
+          console.log(title, "weekNumber", weekNumber);
+        }}
+        className={`size-3 border ${isActive ? "bg-red-500" : "bg-transparent"}`}></div>
+    </Tooltip>
+  );
+}
+
+export default Day;
