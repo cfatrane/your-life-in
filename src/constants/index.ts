e955d6fc43a43157ec43a47a674135e4db65ad06
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-23 22:46:44 +0100

    change color

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ const LIFE_PERIOD = [
     max: 6,
     parent: "École Primaire",
     label: "École Maternelle",
-    color: "bg-green-500",
+    color: "bg-green-800",
   },
   {
     min: 6,
@@ -15,13 +15,19 @@ const LIFE_PERIOD = [
     label: "École Élémentaire",
     color: "bg-green-500",
   },
-  { min: 11, max: 14, label: "Collège", color: "bg-yellow-500" },
-  { min: 14, max: 18, label: "Lycée", color: "bg-pink-500" },
-  { min: 18, max: 21, label: "Université", color: "bg-red-500" },
-  { min: 21, max: 64, label: "Carrière", color: "bg-purple-500" },
+  { min: 11, max: 14, label: "Collège", color: "bg-yellow-400" },
+  { min: 14, max: 18, label: "Lycée", color: "bg-orange-400" },
+  { min: 18, max: 21, label: "Université", color: "bg-purple-500" },
+  { min: 21, max: 64, label: "Carrière", color: "bg-red-500" },
   { min: 64, max: 90, label: "Retraite", color: "bg-black" },
 ];
 const NUMBER_OF_CASE_FOR_WEEKS = 52 * 90;
 const NUMBER_OF_CASE_FOR_YEARS = 9 * 10;
+const NUMBER_OF_WEEKS_BY_YEAR = 52;
 
-export { LIFE_PERIOD, NUMBER_OF_CASE_FOR_WEEKS, NUMBER_OF_CASE_FOR_YEARS };
+export {
+  LIFE_PERIOD,
+  NUMBER_OF_WEEKS_BY_YEAR,
+  NUMBER_OF_CASE_FOR_WEEKS,
+  NUMBER_OF_CASE_FOR_YEARS,
+};
