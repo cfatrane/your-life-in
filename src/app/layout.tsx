238418363116f8e5c4910e6883be18f32c4edb16
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-08-11 01:10:07 +0200

    add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`mx-auto sm:px-6 lg:px-8 ${inter.className}`}>
         {children}
+
+        <Analytics />
       </body>
     </html>
   );
